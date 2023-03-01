a95d3935585050ec36db14f9b0c8c2511d3bf7d3
david zambrano corral <Yayoamigo>
2023-02-28 19:54:51 -0500

    Use serial

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,40 +1,40 @@
 --- Implement the database from the diagram.
 
 CREATE TABLE patients (
-    id INT GENERATED ALWAYS AS IDENTITY,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(100),
     date_of_birth DATE,
-    PRIMARY KEY(id)
+    
 );
 
 CREATE TABLE medical_histories (
-    id INT GENERATED ALWAYS AS IDENTITY,
+    id SERIAL PRIMARY KEY,
     admitted_at TIMESTAMP,
     patient_id INT,
     status VARCHAR(100),
     FOREIGN KEY (patient_id) REFERENCES patients (id),
-    PRIMARY KEY(id)
+    
 );
 
 CREATE TABLE treatments (
-    id INT GENERATED ALWAYS AS IDENTITY,
+    id SERIAL PRIMARY KEY,
     type VARCHAR(100),
     name VARCHAR(100),
-    PRIMARY KEY(id)
+    
 );
 
 CREATE TABLE invoices (
-    id INT GENERATED ALWAYS AS IDENTITY,
+    id SERIAL PRIMARY KEY,
     total_amount DECIMAL,
     generated_at TIMESTAMP,
     payed_at TIMESTAMP,
     medical_history_id INT,
     FOREIGN KEY (medical_history_id) REFERENCES medical_histories (id),
-    PRIMARY KEY(id)
+    
 );
 
 CREATE TABLE invoice_items (
-    id INT GENERATED ALWAYS AS IDENTITY,
+    id SERIAL PRIMARY KEY,
     unit_price DECIMAL,
     quantity INT,
     total_price DECIMAL,
@@ -42,13 +42,13 @@ CREATE TABLE invoice_items (
     treatment_id INT,
     FOREIGN KEY (invoice_id) REFERENCES invoices (id),
     FOREIGN KEY (treatment_id) REFERENCES treatments (id),
-    PRIMARY KEY(id)
+    
 );
 
 --- Join tables from many-to-many relationships might not appear
 
 CREATE TABLE treatments_history (
-    id INT GENERATED ALWAYS AS IDENTITY,
+    id SERIAL PRIMARY KEY,
     medical_history_id INT,
     treatment_id INT,
     FOREIGN KEY (medical_history_id) REFERENCES medical_histories (id),
